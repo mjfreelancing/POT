74bbec0225dad6372a36f276bf73de9012e37f55
Malcolm Smith <malcolm@mjfreelancing.com>
2024-08-18 01:08:04 +1000

    Working on some property to observable creation

--- Source/Maui/Features/Accounts/Views/AccountsPage.xaml.cs
+++ Source/Maui/Features/Accounts/Views/AccountsPage.xaml.cs
@@ -5,9 +5,34 @@ namespace Pot.Maui.Features.Accounts.Views;
 
 public partial class AccountsPage : ViewModelContentPage<AccountsViewModel>
 {
+    private IDisposable? _subscription1;
+
     public AccountsPage(AccountsViewModel viewModel)
         : base(viewModel)
     {
         InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        // The view model may kick off operations when it is activated (when base.OnAppearing() is called),
+        // so observables are setup in advance.
+
+        _subscription1 = ViewModel.BusyChanged
+            .Subscribe(busy =>
+            {
+
+            });
+
+        // Now allow the view model to be 'activated'
+        base.OnAppearing();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        _subscription1!.Dispose();
+        _subscription1 = null;
     }
 }
--- Source/Maui/Mvvm/ViewModelBase.cs
+++ Source/Maui/Mvvm/ViewModelBase.cs
@@ -1,6 +1,11 @@
-﻿using AllOverIt.Patterns.ResourceInitialization;
+﻿using AllOverIt.Extensions;
+using AllOverIt.Patterns.ResourceInitialization;
+using AllOverIt.Reactive.Extensions;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
 
 namespace Pot.Maui.Mvvm;
 
@@ -15,6 +20,11 @@ public partial class ViewModelBase : ObservableObject, IViewModel
 
     public bool IsNotBusy => !IsBusy;
 
+
+    // This is disposable
+    public ObservableProperty<bool> BusyChanged { get; } = new();
+
+
     public IDisposable StartIsBusyScope()
     {
         // TODO: Create a reference counted version so it can handle nested calls
@@ -25,9 +35,105 @@ public partial class ViewModelBase : ObservableObject, IViewModel
 
     public virtual void OnActivate(CompositeDisposable disposables)
     {
+        BindPropertyChangedToObservable<ViewModelBase, bool>(vm => vm.IsBusy, BusyChanged, disposables);
     }
 
     public virtual void OnDeactivate()
     {
+    }
+
+
+    private void BindPropertyChangedToObservable<TViewModel, TProperty>(Expression<Func<TViewModel, TProperty>> propertyExpression,
+        ObservableProperty<TProperty> observableProperty, CompositeDisposable disposables) where TViewModel : ViewModelBase
+    {
+        // An observable that will emit a value when the property value is changed.
+        var observable = new PropertyToObservable<TViewModel, TProperty>((TViewModel)this, propertyExpression);
+
+        // Subscribe to the observable, and forward the emitted value to the observable property.
+        observableProperty.BindTo(observable).DisposeUsing(disposables);
+
+        observable.DisposeUsing(disposables);
+    }
+}
+
+
+
+public sealed class ObservableProperty<TProperty> : IObservable<TProperty>, IDisposable
+{
+    private bool _disposed;
+    private readonly Subject<TProperty> _valueChanged = new();
+
+    public IDisposable Subscribe(IObserver<TProperty> observer)
+    {
+        return _valueChanged.Subscribe(observer);
+    }
+
+    // When the observable emits a value, subscribe for notification
+    public IDisposable BindTo(IObservable<TProperty> observable)
+    {
+        return observable.Subscribe(_valueChanged);
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _valueChanged.Dispose();
+            _disposed = true;
+        }
+    }
+}
+
+
+
+public sealed class PropertyToObservable<TViewModel, TProperty> : IObservable<TProperty>, IDisposable where TViewModel : ViewModelBase
+{
+    private bool _disposed;
+    private readonly Subject<TProperty> _subject = new();
+    private readonly TViewModel _observable;
+    private readonly Func<TViewModel, TProperty> _valueResolver;
+    private readonly string _propertyName;
+
+    public PropertyToObservable(TViewModel observable, Expression<Func<TViewModel, TProperty>> propertyExpression)
+    {
+        _observable = observable;
+        _valueResolver = propertyExpression.Compile();
+        _propertyName = propertyExpression.UnwrapMemberExpression().Member.Name;
+        _observable.PropertyChanged += HandlePropertyChanged;
+    }
+
+    public IDisposable Subscribe(IObserver<TProperty> observer)
+    {
+        return _subject.Subscribe(observer);
+    }
+
+    private void HandlePropertyChanged(object? sender, PropertyChangedEventArgs eventArgs)
+    {
+        if (eventArgs.PropertyName == _propertyName)
+        {
+            var value = _valueResolver.Invoke(_observable);
+
+            _subject.OnNext(value);
+        }
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                _subject.Dispose();
+                _observable.PropertyChanged -= HandlePropertyChanged;
+            }
+
+            _disposed = true;
+        }
+    }
+
+    void IDisposable.Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
