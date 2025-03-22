4614ffc0d4c6318bf64119af7c6517ef81cba09b
Malcolm Smith <malcolm@mjfreelancing.com>
2025-03-22 20:33:50 +1100

    Prettier and linting updates

--- Source/Client/pot-react/eslint.config.js
+++ Source/Client/pot-react/eslint.config.js
@@ -1,32 +1,36 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  {
+    ignores: ['dist/**', 'src/lib/', 'src/components/ui/'],
+  },
   {
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
     ],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['off'], // ['warn', { allowConstantExport: true }]
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
-  }
+  },
 );
