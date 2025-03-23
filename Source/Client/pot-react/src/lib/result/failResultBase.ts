f820ed46c64ac0030b7280f1ce55f5cf24e4c2ef
Malcolm Smith <malcolm@mjfreelancing.com>
2025-03-23 12:18:18 +1100

    Cleanup

--- Source/Client/pot-react/src/lib/result/failResultBase.ts
+++ Source/Client/pot-react/src/lib/result/failResultBase.ts
@@ -1,11 +1,7 @@
 export abstract class FailResultBase {
-  public type?: string;
-  public code?: string;
-  public description?: string;
-
-  constructor(type?: string, code?: string, description?: string) {
-    this.type = type;
-    this.code = code;
-    this.description = description;
-  }
+  constructor(
+    public type?: string,
+    public code?: string,
+    public description?: string,
+  ) {}
 }
