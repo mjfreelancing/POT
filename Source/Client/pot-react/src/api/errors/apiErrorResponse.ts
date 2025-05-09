ca65d90c73932a7b4ed002ca033883e925cb98bb
Malcolm Smith <malcolm@mjfreelancing.com>
2025-05-09 12:02:51 +1000

    Add a custom Etag conflict error message

--- Source/Client/pot-react/src/api/errors/apiErrorResponse.ts
+++ Source/Client/pot-react/src/api/errors/apiErrorResponse.ts
@@ -24,6 +24,11 @@ export const getNotFoundMessage = (_error: ApiErrorResponse): string => {
 export const getConflictMessage = (error: ApiErrorResponse): string => {
   if (error.errors?.length) {
     const firstError = error.errors[0];
+
+    if (firstError.propertyName === 'Etag') {
+      return `The record has been updated by another user. Refresh and try again.`;
+    }
+
     return `The '${firstError.propertyName}' conflicts with another record that has the same value '${firstError.attemptedValue}'.`;
   }
 
