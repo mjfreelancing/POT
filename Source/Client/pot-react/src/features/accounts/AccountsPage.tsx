de92f0218b5e1efdb40cddd8de51f8022caec0c6
Malcolm Smith <malcolm@mjfreelancing.com>
2025-05-23 22:26:18 +1000

    Commented out experimental code - come back to

--- Source/Client/pot-react/src/features/accounts/AccountsPage.tsx
+++ Source/Client/pot-react/src/features/accounts/AccountsPage.tsx
@@ -2,6 +2,13 @@ import { Outlet } from 'react-router';
 
 import { AccountsHeader } from './AccountsHeader';
 import { AccountsTable } from './AccountsTable';
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from '@/components/ui/card';
 
 const AccountsPage = () => {
   console.info('Rendering AccountsPage');
@@ -11,7 +18,24 @@ const AccountsPage = () => {
       <AccountsHeader />
       <div className="flex-1 p-8 pt-6">
         <AccountsTable />
+        {/* EXPERIMENTAL CODE BELOW TO WORK OUT COLUMNS */}
+        {/* <div className="mt-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Title</CardTitle>
+              <CardDescription>Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid grid-cols-2 gap-4 w-full">
+                <AccountsTable />
+                <AccountsTable />
+              </div>
+            </CardContent>
+          </Card>
+        </div> */}
+        {/* EXPERIMENTAL CODE ABOVE TO WORK OUT COLUMNS */}
       </div>
+      {/* The Outlet is used to render nested routes, such as when creating/editing accounts */}
       <Outlet />
     </div>
   );
