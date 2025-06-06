c6e9c8f402b40252c1e1149a18e495a0cd72ac44
Malcolm Smith <malcolm@mjfreelancing.com>
2025-06-06 14:44:42 +1000

    Moved test setup

--- Source/Client/pot-react/vitest.config.ts
+++ Source/Client/pot-react/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./tests/setup.ts'],
   },
   resolve: {
     alias: {
