01a39a3b3d372de9091f010b889ee0ca30bb890e
Malcolm Smith <malcolm@mjfreelancing.com>
2025-07-05 17:42:39 +1000

    Removed unused server code

--- Source/Server/Pot.AspNetCore/Calculators/Allocation/AllocationCalculatorSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pot.AspNetCore.Calculators.Allocation
-{
-    public sealed record AllocationCalculatorSettings
-    {
-        public bool AllowNegativeBalance { get; set; } = true;
-    }
-}
--- Source/Server/Pot.AspNetCore/Calculators/Allocation/IAllocationCalculator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Pot.AspNetCore.Calculators.Allocation
-{
-    public interface IAllocationCalculator
-    {
-        // TODO: Update this so the calculations are applied against the entities
-        void AllocateFunds(/*AccountDto[] accounts, ExpenseDto[] expenses,*/ AllocationCalculatorSettings calculatorSettings);
-    }
-}
