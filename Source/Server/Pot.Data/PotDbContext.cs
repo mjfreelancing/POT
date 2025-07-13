22f47500cdf5e318219cafe01870989600bc927f
Malcolm Smith <malcolm@mjfreelancing.com>
2025-07-13 21:53:02 +1000

    Get database configuration from environment variables (different port for dev)

--- Source/Server/Pot.Data/PotDbContext.cs
+++ Source/Server/Pot.Data/PotDbContext.cs
@@ -1,20 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AllOverIt.Assertion;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Pot.Data.Entities;
 
 namespace Pot.Data
 {
     public sealed class PotDbContext : DbContextBase
     {
+        private readonly IConfiguration _configuration;
+
         public DbSet<AccountEntity> Accounts { get; set; }
         public DbSet<ExpenseEntity> Expenses { get; set; }
+
+        public PotDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration.WhenNotNull();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
 
-            // TODO: Get username / password from environment variables via IConfiguration
-            var databaseHost = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
-            var connectionString = $"Host={databaseHost};Database=Pot;Username=postgres;Password=password";
+            var databaseHost = GetConfigurationValue("DATABASE_HOST");
+            var databaseUsername = GetConfigurationValue("DATABASE_USERNAME");
+            var databasePassword = GetConfigurationValue("DATABASE_PASSWORD");
+
+            var connectionString = $"Host={databaseHost};Database=Pot;Username={databaseUsername};Password={databasePassword}";
 
             optionsBuilder.UseNpgsql(connectionString, options =>
             {
@@ -25,6 +36,11 @@ namespace Pot.Data
         public override void Dispose()
         {
             base.Dispose();
+        }
+
+        private string GetConfigurationValue(string key)
+        {
+            return _configuration[key] ?? throw new InvalidOperationException($"Configuration value for '{key}' is not set.");
         }
     }
 }
