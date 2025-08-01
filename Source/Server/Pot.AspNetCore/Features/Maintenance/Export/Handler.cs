897e659181ccffface7de34fd764484d35c42500
Malcolm Smith <malcolm@mjfreelancing.com>
2025-08-02 09:54:24 +1000

    Update export to use local time for the filename

--- Source/Server/Pot.AspNetCore/Features/Maintenance/Export/Handler.cs
+++ Source/Server/Pot.AspNetCore/Features/Maintenance/Export/Handler.cs
@@ -22,8 +22,8 @@ internal sealed class Handler
         await stream.FlushAsync(cancellationToken);
         stream.Position = 0;
 
-        var timestamp = timeProvider.GetUtcDateTimeNow();
-        var fileName = $"pot-{timestamp:yyyy-MM-dd_HHmmss}-utc.export";
+        var timestamp = timeProvider.GetLocalDateTimeNow();
+        var fileName = $"pot-{timestamp:yyyy-MM-dd_HHmmss}.export";
 
         // The stream is disposed of after the response is sent.
         return TypedResults.File(stream, "application/zip", fileName);
