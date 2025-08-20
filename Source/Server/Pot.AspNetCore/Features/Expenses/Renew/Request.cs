e7e716fafc7685e4b601784540e9a89729121039
Malcolm Smith <malcolm@mjfreelancing.com>
2025-08-20 19:04:55 +1000

    Description updates

--- Source/Server/Pot.AspNetCore/Features/Expenses/Renew/Request.cs
+++ Source/Server/Pot.AspNetCore/Features/Expenses/Renew/Request.cs
@@ -7,6 +7,6 @@ public sealed class Request
     [Description("The Expense Ids.")]
     public Guid[] RowIds { get; init; } = [];
 
-    [Description("The expenses will be renewed until the next due date exceeds this date.")]
+    [Description("The expenses will be renewed to the next due date exceeds this date.")]
     public required DateOnly UntilDate { get; init; }
 }
--- Source/Server/Pot.AspNetCore/Features/Incomes/Renew/Request.cs
+++ Source/Server/Pot.AspNetCore/Features/Incomes/Renew/Request.cs
@@ -7,6 +7,6 @@ public sealed class Request
     [Description("The Income Ids.")]
     public Guid[] RowIds { get; init; } = [];
 
-    [Description("The incomes will be renewed until the next due date exceeds this date.")]
+    [Description("The incomes will be renewed to the next due date exceeds this date.")]
     public required DateOnly UntilDate { get; init; }
 }
