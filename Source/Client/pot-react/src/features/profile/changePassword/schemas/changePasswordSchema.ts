52f4f41f4be7bb704b3783c346f4b8cbf8a15496
Malcolm Smith <malcolm@mjfreelancing.com>
2025-09-21 17:08:33 +1000

    change password form validation updates

--- Source/Client/pot-react/src/features/profile/changePassword/schemas/changePasswordSchema.ts
+++ Source/Client/pot-react/src/features/profile/changePassword/schemas/changePasswordSchema.ts
@@ -9,6 +9,10 @@ const changePasswordSchema = z
   .refine(data => data.newPassword === data.confirmPassword, {
     message: 'Passwords do not match',
     path: ['confirmPassword'],
+  })
+  .refine(data => data.currentPassword !== data.newPassword, {
+    message: 'New password must be different from current password',
+    path: ['newPassword'],
   });
 
 export type ChangePasswordFields = z.infer<typeof changePasswordSchema>;
