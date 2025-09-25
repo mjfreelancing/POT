917280b6c6e497bb25a75da513f5350811f00037
Malcolm Smith <malcolm@mjfreelancing.com>
2025-09-25 23:03:35 +1000

    Comment update

--- Source/Client/pot-react/src/features/auth/usePermissions.ts
+++ Source/Client/pot-react/src/features/auth/usePermissions.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react';
 import { useUserStore } from './stores/useUserStore';
 
 function usePermissions() {
-  // Using this to avoid warning 'The 'permissions' logical expression could make the dependencies of useCallback Hook (at line 12) change on every render.'
+  // Using this to avoid warning 'The 'permissions' logical expression could make the dependencies of useCallback Hook change on every render.'
   const EMPTY_ARRAY: string[] = [];
 
   const permissions =
