a3345ce26f7e51e5e46fdfec81b58a91b9cbddf7
Malcolm Smith <malcolm@mjfreelancing.com>
2025-10-10 23:12:58 +1100

    Add role/permission to entity - length now 50

--- Source/Server/Pot.Data/Entities/PermissionEntity.cs
+++ Source/Server/Pot.Data/Entities/PermissionEntity.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Pot.Data.Annotations;
-using System.ComponentModel.DataAnnotations;
+using Pot.Shared;
 
 namespace Pot.Data.Entities;
 
 [Index(nameof(Name), IsUnique = true)]
 public sealed class PermissionEntity : EntityBase
 {
-    [Required]
-    [MediumString]
-    public required string Name { get; set; }
+    public required Permission Name { get; set; }
 
     // Not strictly necessary (since not used), but makes it clearer this is part of a many-to-many relationship
     public ICollection<RoleEntity> Roles { get; set; } = [];   // Skip navigation property (skips join table)
--- Source/Server/Pot.Data/Entities/RoleEntity.cs
+++ Source/Server/Pot.Data/Entities/RoleEntity.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Pot.Data.Annotations;
-using System.ComponentModel.DataAnnotations;
+using Pot.Shared;
 
 namespace Pot.Data.Entities;
 
 [Index(nameof(Name), IsUnique = true)]
 public sealed class RoleEntity : EntityBase
 {
-    [Required]
-    [MediumString]
-    public required string Name { get; set; }
+    public required Role Name { get; set; }
 
     public ICollection<UserEntity> Users { get; set; } = [];                // Skip navigation property (skips join table)
     public ICollection<PermissionEntity> Permissions { get; set; } = [];    // Skip navigation property (skips join table)
