9ff699645b5644954bdc4d9494a96bff2f873be2
Malcolm Smith <malcolm@mjfreelancing.com>
2025-11-04 20:48:21 +1100

    Add database Port and SSLMode

--- Source/Server/Pot.Data.Migrations/PotDbContextFactory.cs
+++ Source/Server/Pot.Data.Migrations/PotDbContextFactory.cs
@@ -42,8 +42,10 @@ public class PotDbContextFactory : IDesignTimeDbContextFactory<PotDbContext>
         var databaseHost = GetConfigurationValue(configuration, "Database:Host");
         var databaseUsername = GetConfigurationValue(configuration, "Database:Username");
         var databasePassword = GetConfigurationValue(configuration, "Database:Password");
+        var databasePort = GetConfigurationValue(configuration, "Database:Port");
+        var databaseSslMode = GetConfigurationValue(configuration, "Database:Password");
 
-        return $"Host={databaseHost};Database={databaseName};Username={databaseUsername};Password={databasePassword}";
+        return $"Host={databaseHost};Database={databaseName};Username={databaseUsername};Password={databasePassword};Port={databasePort};SSLMode={databaseSslMode}";
     }
 
     private static string GetConfigurationValue(IConfiguration configuration, string key)
