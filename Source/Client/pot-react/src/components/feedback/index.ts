548921cf0ac1ff1ba31b65b7f1ba688ba701f3e4
Malcolm Smith <malcolm@mjfreelancing.com>
2025-11-08 14:55:43 +1100

    Lint sort

--- Source/Client/pot-react/src/components/feedback/index.ts
+++ Source/Client/pot-react/src/components/feedback/index.ts
@@ -1,7 +1,7 @@
 export * from './badge';
 export * from './message';
+export { default as LoadingMessage } from './message/LoadingMessage';
 export { default as NotePopover } from './popover/NotePopover';
 export { default as ErrorSheet } from './sheet/ErrorSheet';
-export { default as LoadingMessage } from './message/LoadingMessage';
 export { default as LoadingOverlay } from './spinner/LoadingOverlay';
 export { default as LoadingSpinner } from './spinner/LoadingSpinner';
