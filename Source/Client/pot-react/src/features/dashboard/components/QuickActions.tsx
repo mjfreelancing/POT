49695a0a5edbfab77f80fdf386874be25c7158f1
Malcolm Smith <malcolm@mjfreelancing.com>
2026-02-05 22:50:53 +1100

    Update QuickActions to use 2 columns on mobile and 4 on desktop

--- Source/Client/pot-react/src/features/dashboard/components/QuickActions.tsx
+++ Source/Client/pot-react/src/features/dashboard/components/QuickActions.tsx
@@ -22,7 +22,7 @@ function QuickActions() {
       />
       <CardContent className="px-4 -mt-2">
         <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4">
             <AccrualsProvider>
               <PermissionGuard permissions={['expense:manage']} mode="all">
                 <RenewExpensesAction />
