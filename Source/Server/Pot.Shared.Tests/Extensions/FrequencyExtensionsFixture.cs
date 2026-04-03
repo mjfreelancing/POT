ff491d3d9489785ff7560bccb94ad629d3dd728e
Malcolm Smith <malcolm@mjfreelancing.com>
2026-04-03 11:13:12 +1100

    Add more frequency related tests

--- Source/Server/Pot.Shared.Tests/Extensions/FrequencyExtensionsFixture.cs
+++ Source/Server/Pot.Shared.Tests/Extensions/FrequencyExtensionsFixture.cs
@@ -2,6 +2,7 @@
 using Pot.Shared.Extensions;
 using Pot.TestUtils;
 using Shouldly;
+using Xunit;
 
 namespace Pot.Shared.Tests.Extensions;
 
@@ -9,6 +10,49 @@ public class FrequencyExtensionsFixture : PotFixtureBase
 {
     public class GetDaysToNext : FrequencyExtensionsFixture
     {
+        public static IEnumerable<object[]> EndOfMonthMonthlyTraversalCases()
+        {
+            yield return
+            [
+                new DateOnly(2023, 12, 31),
+                new[]
+                {
+                    new DateOnly(2024, 1, 31),
+                    new DateOnly(2024, 2, 29),
+                    new DateOnly(2024, 3, 31),
+                    new DateOnly(2024, 4, 30),
+                    new DateOnly(2024, 5, 31),
+                    new DateOnly(2024, 6, 30),
+                    new DateOnly(2024, 7, 31),
+                    new DateOnly(2024, 8, 31),
+                    new DateOnly(2024, 9, 30),
+                    new DateOnly(2024, 10, 31),
+                    new DateOnly(2024, 11, 30),
+                    new DateOnly(2024, 12, 31),
+                    new DateOnly(2025, 1, 31),
+                    new DateOnly(2025, 2, 28),
+                    new DateOnly(2025, 3, 31),
+                },
+            ];
+        }
+
+        public static IEnumerable<object[]> EndOfMonthQuarterlyTraversalCases()
+        {
+            yield return
+            [
+                new DateOnly(2023, 12, 31),
+                new[]
+                {
+                    new DateOnly(2024, 3, 31),
+                    new DateOnly(2024, 6, 30),
+                    new DateOnly(2024, 9, 30),
+                    new DateOnly(2024, 12, 31),
+                    new DateOnly(2025, 3, 31),
+                    new DateOnly(2025, 6, 30),
+                },
+            ];
+        }
+
         [Fact]
         public void Should_Return_Frequency_Count_For_Days()
         {
@@ -55,6 +99,42 @@ public class FrequencyExtensionsFixture : PotFixtureBase
             var result = Frequency.EndOfMonth.GetDaysToNext(new DateOnly(2025, 1, 31), 2);
 
             result.ShouldBe(59);
+        }
+
+        [Theory]
+        [MemberData(nameof(EndOfMonthMonthlyTraversalCases))]
+        public void Should_Advance_EndOfMonth_One_Month_At_A_Time_Through_Leap_And_NonLeap_Februaries(
+            DateOnly startDate,
+            IReadOnlyList<DateOnly> expectedDates)
+        {
+            var currentDate = startDate;
+
+            foreach (var expectedDate in expectedDates)
+            {
+                var daysToNext = Frequency.EndOfMonth.GetDaysToNext(currentDate, 1);
+
+                currentDate = currentDate.AddDays(daysToNext);
+
+                currentDate.ShouldBe(expectedDate);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(EndOfMonthQuarterlyTraversalCases))]
+        public void Should_Advance_EndOfMonth_Three_Months_At_A_Time(
+            DateOnly startDate,
+            IReadOnlyList<DateOnly> expectedDates)
+        {
+            var currentDate = startDate;
+
+            foreach (var expectedDate in expectedDates)
+            {
+                var daysToNext = Frequency.EndOfMonth.GetDaysToNext(currentDate, 3);
+
+                currentDate = currentDate.AddDays(daysToNext);
+
+                currentDate.ShouldBe(expectedDate);
+            }
         }
 
         [Fact]
